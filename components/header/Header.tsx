a1a9389841a5808880f3d47449bedfc3aa406545
Matheus Aparecido <matheusaparecido101@gmail.com>
2024-05-05 00:08:27 +0000

    Release staging environment with 0 changes made by

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,21 +1,29 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
+interface Button {
+  label: string;
+  href: string;
+  /**
+   * @format color
+   */
+  color: string;
+}
+
 export interface Props {
   logo: {
     alt: string;
     src: ImageWidget;
   };
+  buttons: Button[];
 }
 
 export default function Header(props: Props) {
   return (
-    <header
-      class={"w-full h-20 flex flex-row justify-between bg-[#1F70B8] px-14"}
-    >
+    <header class={"w-full h-20 flex flex-row justify-between bg-[#1F70B8]"}>
       <div class="w-auto h-full flex justify-center items-center">
         <Image
-          width={150}
+          width={300}
           height={60}
           fetchPriority="high"
           loading="eager"
@@ -24,23 +32,15 @@ export default function Header(props: Props) {
           alt={props.logo.alt}
         />
       </div>
-      <nav class="flex justify-center items-center h-full">
+      <nav>
         <ul class="flex flex-row gap-3 justify-center items-center">
-          <li style={{ background: "#EDC61C" }} class="px-3 py-2 rounded-lg">
-            <a href={"/salas"} class="text-black uppercase font-semibold">
-              Salas
-            </a>
-          </li>
-          <li style={{ background: "#1CBBED" }} class="px-3 py-2 rounded-lg">
-            <a href={"/professores"} class="text-black uppercase font-semibold">
-              Professores
-            </a>
-          </li>
-          <li style={{ background: "#FF0000" }} class="px-3 py-2 rounded-lg">
-            <a href={"/login"} class="text-black uppercase font-semibold">
-              Sair
-            </a>
-          </li>
+          {props.buttons.map((button) => (
+            <li style={{ background: button.color }} class="px-3 py-2">
+              <a href={button.href} class="text-blac uppercase">
+                {button.label}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
