1c72b7192418d711cc55f008ab33dc31842bf326
ApMatheus <matheusaparecido101@gmail.com>
2024-05-12 11:40:27 -0300

    fix: header links

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -19,7 +19,10 @@ export default function Header(props: SectionProps<ReturnType<typeof loader>>) {
     <header
       class={"w-full h-20 flex flex-row justify-between bg-[#1F70B8] px-14"}
     >
-      <div class="w-auto h-full flex justify-center items-center">
+      <a
+        href="/historico"
+        class="w-auto h-full flex justify-center items-center"
+      >
         <Image
           width={150}
           height={60}
@@ -29,21 +32,27 @@ export default function Header(props: SectionProps<ReturnType<typeof loader>>) {
           src={props.logo.src}
           alt={props.logo.alt}
         />
-      </div>
+      </a>
       <nav class="flex justify-center items-center h-full">
         <ul class="flex flex-row gap-3 justify-center items-center">
           <li style={{ background: "#EDC61C" }} class="px-3 py-2 rounded-lg">
-            <a href={"/salas"} class="text-black uppercase font-semibold">
+            <a
+              href={"/historico-salas"}
+              class="text-black uppercase font-semibold"
+            >
               Salas
             </a>
           </li>
           <li style={{ background: "#1CBBED" }} class="px-3 py-2 rounded-lg">
-            <a href={"/professores"} class="text-black uppercase font-semibold">
+            <a
+              href={"/historico-professores"}
+              class="text-black uppercase font-semibold"
+            >
               Professores
             </a>
           </li>
           <li style={{ background: "#FF0000" }} class="px-3 py-2 rounded-lg">
-            <a href={"/login"} class="text-black uppercase font-semibold">
+            <a href={"/"} class="text-black uppercase font-semibold">
               Sair
             </a>
           </li>
